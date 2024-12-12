3d4b0e8248da17b4f9347091ccde2f287869a3d3
Sapomatto <L20170679@culiacan.tecnm.mx>
2024-12-12 11:11:02 -0700

    1

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,6 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestor de Recetas</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
